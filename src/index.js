9a6364fa28595fc192a714f66ca471314913cc6d
timotheus ingga pranata <trancexe@gmail.com>
2022-07-17 12:06:49 +0900

    Revert "test login page"
    This reverts commit d0a4113a8bba9ef92beb2dbff81fec11d895f846.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./containers/LoginPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
